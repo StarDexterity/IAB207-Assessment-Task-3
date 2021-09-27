df1ed0821d73f6ab74d39f4adf5b83238c8bae03
Zachary Torr <ztorr1@hotmail.com>
2021-09-27 20:48:59 +1000

    Integrated website into application

--- website/__init__.py
+++ website/__init__.py
@@ -25,8 +25,8 @@ def create_app():
     
     #set the name of the login function that lets user login
     # in our case it is auth.login (blueprintname.viewfunction name)
-    login_manager.login_view='auth.login'
-    login_manager.init_app(app)
+    #login_manager.login_view='auth.login'
+    #login_manager.init_app(app)
 
     #create a user loader function takes userid and returns User
     #from .models import User  # importing here to avoid circular references
--- website/views.py
+++ website/views.py
@@ -1,8 +1,22 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, url_for
 
 bp = Blueprint('main', __name__)
 
 
 @bp.route('/')
 def index():
-    return '<h1>Starter code for the assessment<h1>'
+    return render_template('index.html')
+
+@bp.route('/create-event')
+def create_event():
+    return render_template('create_event.html')
+
+
+@bp.route('/booked-events')
+def booked_events():
+    return render_template('booked_events.html')
+
+
+@bp.route('/view-details')
+def view_details():
+    return render_template('view_details.html')
