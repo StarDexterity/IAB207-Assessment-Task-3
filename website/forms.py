eb81c42a211fd8a7b799866249435916bcf49153
JoshuaBryans <91448221+JoshuaBryans@users.noreply.github.com>
2021-10-13 11:41:46 +1000

    adding_validators

--- website/forms.py
+++ website/forms.py
@@ -17,8 +17,8 @@ class RegisterForm(FlaskForm):
     user_name=StringField("User Name", validators=[InputRequired()])
     email_id = StringField("Email Address", validators=[Email("Please enter a valid email")])
 
-    phone_no = StringField('Phone number')
-    address = StringField('Address')
+    phone_no = StringField('Phone number', validators=[InputRequired()])
+    address = StringField('Address', validators=[InputRequired()])
     
     #linking two fields - password should be equal to data entered in confirm
     password=PasswordField("Password", validators=[InputRequired(),
