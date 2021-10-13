750d86bd254590f9f5c03f1e2908bed70133a733
JoshuaBryans <91448221+JoshuaBryans@users.noreply.github.com>
2021-10-13 12:31:36 +1000

    adding constraints to register

--- website/forms.py
+++ website/forms.py
@@ -1,7 +1,7 @@
 
 from flask_wtf import FlaskForm
 from wtforms.fields import TextAreaField,SubmitField, StringField, PasswordField, BooleanField
-from wtforms.validators import InputRequired, Length, Email, EqualTo
+from wtforms.validators import InputRequired, Length, Email, EqualTo, ValidationError
 
 
 #creates the login information
@@ -12,13 +12,17 @@ class LoginForm(FlaskForm):
     keep_loggedin = BooleanField("Stay logged in")
     submit = SubmitField("Login")
 
+def my_length_check(form, field):
+    if len(field.data) > 20:
+        raise ValidationError('Username must be less than 20 characters')
+
  # this is the registration form
 class RegisterForm(FlaskForm):
-    user_name=StringField("User Name", validators=[InputRequired()])
+    user_name=StringField("User Name", validators=[InputRequired(), my_length_check])
     email_id = StringField("Email Address", validators=[Email("Please enter a valid email")])
 
-    phone_no = StringField('Phone number', validators=[InputRequired()])
-    address = StringField('Address', validators=[InputRequired()])
+    phone_no = StringField('Phone number', validators=[InputRequired("Please enter a phone number")])
+    address = StringField('Address', validators=[InputRequired("Please enter an address")])
     
     #linking two fields - password should be equal to data entered in confirm
     password=PasswordField("Password", validators=[InputRequired(),
